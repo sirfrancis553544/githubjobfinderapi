5ff2e1b13e3f217b02b263ee469a4458ad811e0f
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-11-26 23:11:58 +0800

    app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
-
+import useFetchJobs from "./useFetchJobs";
 function App() {
+  const { jobs, loading, error } = useFetchJobs();
+
   return <h1>Hello World</h1>;
 }
 
 export default App;
+  
